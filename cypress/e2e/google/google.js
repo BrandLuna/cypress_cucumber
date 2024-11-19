c239c3ab78e79f062161c99d023407c0bed5b99d
Brandon Luna <brandonf.luna@gmail.com>
2024-11-18 19:05:01 -0500

    delete

--- cypress/e2e/google/google.js
+++
@@ -1,9 +0,0 @@
-import { Given, Then } from "@badeball/cypress-cucumber-preprocessor";
-
-Given("I open Google page", () => {
-  cy.visit("https://google.com");
-});
-
-Then("I see Google in the title", () => {
-  cy.title().should("include", "Google");
-});
